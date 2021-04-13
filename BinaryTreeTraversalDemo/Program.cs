5db3dea2816ec06e48abe89d4f99b66b56205248
hussainpatel <hussain.patel@hotmail.com>
2021-04-12 19:51:12 -0700

    renamed - the solution file and added new project

---
+++ BinaryTreeTraversalDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinaryTreeTraversalDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ BinaryTreeTraversalDemo/Tree.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTreeTraversalDemo
+{
+    public class Tree
+    {
+        private Node root;
+
+
+        public void Traversal()
+        {
+            
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private class Node
+        {
+            protected int value;
+            protected Node leftChild;
+            protected Node rightChild;
+
+            public Node(int value)
+            {
+                this.value = value;
+            }
+        }
+    }
+}
